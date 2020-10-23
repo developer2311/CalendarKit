50ef6188b0b4c1e4f7138d935e0396cefb4de2dd
Nikita Lazarev <application2311@gmail.com>
2020-10-23 15:23:47 +0300

    Add public func to setup event views configuration

--- Source/Timeline/EventViewConfiguration.swift
+++ Source/Timeline/EventViewConfiguration.swift
@@ -8,5 +8,13 @@ public struct EventViewConfiguration {
     var shouldRoundCorners = false
     // - Set horizontal insets for textView inside event view. Works ! ONLY ! if shouldEnableResizing = false and shouldRoundCorners = true -
     var textViewHorizontalInsets: CGFloat = 15.0
+    
+    public func setup(_ shouldEnableRezising: Bool,
+                      shouldRoundCorners: Bool,
+                      textViewHorizontalInsets: CGFloat) {
+        self.shouldEnableRezising = shouldEnableRezising
+        self.shouldRoundCorners = shouldRoundCorners
+        self.textViewHorizontalInsets = textViewHorizontalInsets
+    }
 }
 #endif
