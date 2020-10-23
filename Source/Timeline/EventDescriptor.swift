e94e83007d92b51db5fd64b2be83cbc037f4fd93
Nikita Lazarev <application2311@gmail.com>
2020-10-23 18:47:17 +0300

    Added missing properties

--- Source/Timeline/EventDescriptor.swift
+++ Source/Timeline/EventDescriptor.swift
@@ -3,17 +3,18 @@ import Foundation
 import UIKit
 
 public protocol EventDescriptor: AnyObject {
-  var startDate: Date {get set}
-  var endDate: Date {get set}
-  var isAllDay: Bool {get}
-  var text: String {get}
-  var attributedText: NSAttributedString? {get}
-  var font : UIFont {get}
-  var color: UIColor {get}
-  var textColor: UIColor {get}
-  var backgroundColor: UIColor {get}
-  var editedEvent: EventDescriptor? {get set}
-  func makeEditable() -> Self
-  func commitEditing()
+    var startDate: Date {get set}
+    var endDate: Date {get set}
+    var isAllDay: Bool {get}
+    var text: String {get}
+    var attributedText: NSAttributedString? {get}
+    var font : UIFont {get}
+    var color: UIColor {get}
+    var textColor: UIColor {get}
+    var backgroundColor: UIColor {get}
+    var isEventPinned: Bool {get set}
+    var editedEvent: EventDescriptor? {get set}
+    func makeEditable() -> Self
+    func commitEditing()
 }
 #endif
