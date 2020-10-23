023097dd9d1ea7d3a2c9ae540e5630ddb8354d7f
Nikita Lazarev <application2311@gmail.com>
2020-10-23 15:30:31 +0300

    Implemented public init for EventViewConfiguration

--- Source/Timeline/EventViewConfiguration.swift
+++ Source/Timeline/EventViewConfiguration.swift
@@ -2,19 +2,19 @@
 import UIKit
 
 public struct EventViewConfiguration {
-    // - Enables ability to resize event view -
-    var shouldEnableRezising = true
-    // - Makes event view rounded -
-    var shouldRoundCorners = false
-    // - Set horizontal insets for textView inside event view. Works ! ONLY ! if shouldEnableResizing = false and shouldRoundCorners = true -
-    var textViewHorizontalInsets: CGFloat = 15.0
-    
-    public func setup(_ shouldEnableRezising: Bool,
-                      shouldRoundCorners: Bool,
-                      textViewHorizontalInsets: CGFloat) {
+    public init(_ shouldEnableRezising: Bool, shouldRoundCorners: Bool, textViewHorizontalInsets: CGFloat = .zero) {
         self.shouldEnableRezising = shouldEnableRezising
         self.shouldRoundCorners = shouldRoundCorners
-        self.textViewHorizontalInsets = textViewHorizontalInsets
+        if textViewHorizontalInsets != .zero {
+            self.textViewHorizontalInsets = textViewHorizontalInsets
+        }
     }
+    
+    // - Enables ability to resize event view -
+    var shouldEnableRezising: Bool = true
+    // - Makes event view rounded -
+    var shouldRoundCorners: Bool = false
+    // - Set horizontal insets for textView inside event view. Works ! ONLY ! if shouldEnableResizing = false and shouldRoundCorners = true -
+    var textViewHorizontalInsets: CGFloat = 15.0
 }
 #endif
